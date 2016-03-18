952a18e432d35c8648221516517bcfb275681f16
Hinekureta <herrbach.kevin@gmail.com>
2016-03-18 12:16:35 +0100

    Commit Ex3 Done

--- exercises/nodejs/03-promise/index.js
+++ exercises/nodejs/03-promise/index.js
@@ -44,7 +44,7 @@ function askUser() {
       }
     ], function (choices) {
       console.log(choices);
-
+      resolve(choices);
       // TODO resolve the promise !!!
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
     });
@@ -60,9 +60,19 @@ function fetchData(choices) {
   const spinner = ora('Fetching StarWars API...');
   spinner.start();
 
+  return fetch(url)
+    .then(function onResponse(response) {
+      if(response.ok)
+      {
+        spinner.stop();
+        return response.json();
+      }
+      else
+        throw new Error('Network response was not ok.');
+      });
   // TODO now use the fetch API :
   // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch#Checking_that_the_fetch_was_successful
-  return Promise.reject(new Error('fetchData not implemented !'));
+  //return Promise.reject(new Error('fetchData not implemented !'));
 }
 
 function displayResults(data) {
