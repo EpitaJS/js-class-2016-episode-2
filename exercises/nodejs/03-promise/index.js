d40816b18a607ab569978ef193cec5c554249388
Leaxus <alx.phonesavanh@gmail.com>
2016-03-18 11:27:12 +0100

    ex3

--- exercises/nodejs/03-promise/index.js
+++ exercises/nodejs/03-promise/index.js
@@ -47,6 +47,7 @@ function askUser() {
 
       // TODO resolve the promise !!!
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+      resolve(choices);
     });
   });
 }
@@ -62,7 +63,12 @@ function fetchData(choices) {
 
   // TODO now use the fetch API :
   // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch#Checking_that_the_fetch_was_successful
-  return Promise.reject(new Error('fetchData not implemented !'));
+      return fetch(url)
+        .then(function(res) {
+            spinner.stop();
+            return res.json();
+        });
+  //return Promise.reject(new Error('fetchData not implemented !'));
 }
 
 function displayResults(data) {
