a4c6cec9d6e377e0ab446daffc739b4ed6430d2a
Hinekureta <herrbach.kevin@gmail.com>
2016-03-18 12:51:50 +0100

    Start to modify api

--- exercises/nodejs/05-express_routing/api/index.js
+++ exercises/nodejs/05-express_routing/api/index.js
@@ -14,7 +14,21 @@ const router = module.exports = new express.Router();
 /////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-	res.send('hello from API sub-router !');
+	res.send(`
+<!DOCTYPE html>
+<head>
+  <title>meta routes</title>
+  <style type="text/css">
+    body {
+      margin: 40px;
+      font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+      color: #333;
+    }
+  </style>
+</head>
+<h1>COUCOU</h1>   
+`);
+
   // TODO a small page listing your endpoints
   // cf. js-class-2016-episode-2\src\server\common\meta-routes.js
 });
