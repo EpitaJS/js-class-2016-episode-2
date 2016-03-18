18c609ac4608fef4a207fb7c80bfcb8bfb7a89b4
Dat Florentin <floren_d@epita.fr>
2016-03-18 12:17:11 +0100

    ex 3 done

--- exercises/nodejs/03-promise/index.js
+++ exercises/nodejs/03-promise/index.js
@@ -44,7 +44,7 @@ function askUser() {
       }
     ], function (choices) {
       console.log(choices);
-
+      resolve(choices);
       // TODO resolve the promise !!!
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
     });
@@ -61,8 +61,16 @@ function fetchData(choices) {
   spinner.start();
 
   // TODO now use the fetch API :
+  return fetch(url)
+    .then(function onResponse(response) {
+      spinner.stop();
+      if(response.ok)
+        return response.json();
+      else
+        throw new Error('Network response was not ok.');
+    })
   // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch#Checking_that_the_fetch_was_successful
-  return Promise.reject(new Error('fetchData not implemented !'));
+  //return Promise.reject(new Error('fetchData not implemented !'));
 }
 
 function displayResults(data) {
