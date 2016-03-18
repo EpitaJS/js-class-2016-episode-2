f60d3917b99a2cc83a4a5e0ff710ad60d351f173
Thibaut Fumaneri <fumane_t@epita.fr>
2016-03-18 13:45:38 +0100

    fix server main.html

--- src/server/web/index.js
+++ src/server/web/index.js
@@ -30,31 +30,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(__dirname + '/../../client'));
 
-app.get('/', (req, res) => res.send(`
-<!DOCTYPE html>
-<head>
-	<title>Node.js server</title>
-	<style type="text/css">
-		body {
-			margin: 40px;
-			font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-			color: #333;
-		}
-	</style>
-</head>
-
-<h1>...</h1>
-<li><a>${req.baseUrl}/chat</a>
-<li><a>${req.baseUrl}/api</a>
-<li><a>${req.baseUrl}/meta</a>
-
-<script>
-	document.querySelector('h1').textContent = document.title;
-	Array.prototype.forEach.call(document.querySelectorAll('a'), function(el) {
-		el.href || (el.href = el.text);
-	});
-</script>
-`));
+app.get('/', (req, res) => res.sendfile('main.html'););
 app.use('/chat', chat_router);
 app.use('/api', api_router);
 app.use('/meta', meta_router);
