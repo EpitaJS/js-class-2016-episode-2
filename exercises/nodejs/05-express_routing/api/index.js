74e3a78651320aa28d4d0007b819119bc89d7a5f
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-03-18 11:17:08 +0100

    ex5: add api routes.

--- exercises/nodejs/05-express_routing/api/index.js
+++ exercises/nodejs/05-express_routing/api/index.js
@@ -14,9 +14,11 @@ const router = module.exports = new express.Router();
 /////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-	res.send('hello from API sub-router !');
+//	res.send('hello from API sub-router !');
   // TODO a small page listing your endpoints
   // cf. js-class-2016-episode-2\src\server\common\meta-routes.js
+  var routes = router.stack
+  res.send(routes)
 });
 
 
@@ -24,7 +26,40 @@ router.get('/', (req, res) => {
 // TODO one or two routes
 // be creative !
 
+router.get('/repo/forks', function (req, res) {
+	const fetch = require('node-fetch'); // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 
+	fetch('https://api.github.com/repos/EpitaJS/js-class-2016-episode-2/forks', [])
+	.then(function (response) {
+		if (response.ok)
+		{
+			response.json().then(function (data) {
+				console.log(data);
+				res.send(data);
+			});
+		}
+		else
+			throw new Error('Network response was not ok.');
+	})
+});
+
+router.get('/repo/commits', function (req, res) {
+	const fetch = require('node-fetch'); // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
+	fetch('https://api.github.com/repos/MrCalen/js-class-2016-episode-2/commits', [])
+	.then(function (response) {
+		if (response.ok)
+		{
+			response.json().then(function (data) {
+				console.log(data);
+				res.send(data);
+			});
+		}
+		else
+			throw new Error('Network response was not ok.');
+	})
+	console.log('here');
+});
 
 ////////////////// examples //////////////
 
