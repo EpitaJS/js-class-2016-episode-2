595eeede6072d431d880be262a7dc8593120ac4b
Leaxus <alx.phonesavanh@gmail.com>
2016-03-23 00:23:57 +0100

    ex5

--- exercises/nodejs/05-express_routing/api/index.js
+++ exercises/nodejs/05-express_routing/api/index.js
@@ -14,25 +14,50 @@ const router = module.exports = new express.Router();
 /////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-	res.send('hello from API sub-router !');
   // TODO a small page listing your endpoints
   // cf. js-class-2016-episode-2\src\server\common\meta-routes.js
+	res.send(`
+<!DOCTYPE html>
+<head>
+	<title>Api routes</title>
+	<style type="text/css">
+		body {
+			margin: 40px;
+			font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+			color: #333;
+		}
+	</style>
+</head>
+
+<h1>...</h1>
+<p>
+    Routes : /add/x1/x2
+            /multiply/x1/x2
+</p>
+
+<script>
+	document.querySelector('h1').textContent = document.title;
+	Array.prototype.forEach.call(document.querySelectorAll('a'), function(el) {
+		el.href || (el.href = el.text);
+	});
+</script>
+	`);
 });
 
 
+router.get('/add/:x1/:x2', function (req, res) {
+    var x = parseInt(req.params.x1) + parseInt(req.params.x2);
+    var result = x.toString();
+    res.send(result);
+});
 
-// TODO one or two routes
-// be creative !
-
-
+router.get('/multiply/:x1/:x2', function (req, res) {
+    var x = parseInt(req.params.x1) * parseInt(req.params.x2);
+    var result = x.toString();
+    res.send(result);
+});
 
 ////////////////// examples //////////////
-
-router.get('/hello/:name', function (req, res) {
-  res.send(`Hello, ${req.name} !`);
-});
-
-
 router.get('/stuff/:id', function (req, res) {
 
   res.status(500).json({ error: 'not implemented !' })
