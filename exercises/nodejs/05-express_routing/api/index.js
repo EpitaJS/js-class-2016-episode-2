8df32f572106790267585683028c84f8ef593f18
Ashley KHALEF <ashley.khalef@epita.fr>
2016-03-29 09:31:24 +0200

    All done

--- exercises/nodejs/05-express_routing/api/index.js
+++ exercises/nodejs/05-express_routing/api/index.js
@@ -14,15 +14,50 @@ const router = module.exports = new express.Router();
 /////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-	res.send('hello from API sub-router !');
-  // TODO a small page listing your endpoints
-  // cf. js-class-2016-episode-2\src\server\common\meta-routes.js
+res.send('hello from API sub-router !');
+<!DOCTYPE html>
+ <head>
+ 	<title>meta routes</title>
+ 	<style type="text/css">
+ 		body {
+ 			margin: 40px;
+ 			font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+ 			color: #333;
+ 		}
+ 	</style>
+ </head>
+ 
+ <h1>...</h1>
+ <li><a>${req.baseUrl}/hello</a>
+ <li><a>${req.baseUrl}/are</a>
+ <li><a>${req.baseUrl}/you</a>
+ <li><a>${req.baseUrl}/ready</a>
+ 
+ <script>
+ 	document.querySelector('h1').textContent = document.title;
+ 	Array.prototype.forEach.call(document.querySelectorAll('a'), function(el) {
+ 		el.href || (el.href = el.text);
+ 	});
+ </script>
+ `);
+ });
+
+
+router.get('/are', function(req, res) {
+   res.send(`<img src="https://latimesherocomplex.files.wordpress.com/2010/03/6a00d8341c630a53ef0120a928a6f5970b-600wi.jpg">`)
+});
+  
+router.get('/you', function(req, res) {
+   res.send(`<img src="http://img.over-blog-kiwi.com/0/02/84/12/20140115/ob_1522c2_patate-femme.jpg">`)
 });
 
+router.get('/ready', function(req, res) {
+   res.send(`<img src="http://vignette1.wikia.nocookie.net/aliceinwonderland/images/0/02/Chessur.jpg">`)
+});
 
-
-// TODO one or two routes
-// be creative !
+router.get('/hello', function (req, res) {
+   res.send('Hello :)');
+});
 
 
 
